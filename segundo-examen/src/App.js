ead649e538b5384c92b1cf991348c9f7270bb1ee
MRErnest-tech <316255589@pcpuma.acatlan.unam.mx>
2022-06-02 23:50:58 -0500

    Sigue sin funcionar

--- segundo-examen/src/App.js
+++ segundo-examen/src/App.js
@@ -6,22 +6,20 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 function App() {
-  const [post,updPst] = useState([]);
-  const [imgs, updImgs] = useState([]);
+  const [posts,updAl] = useState([]);
+  const [imagenes, updPro] = useState([]);
   
-  const getPostList = async () => 
+  const getPosts = async () => 
   {
     try {
       let url1 = "https://jsonplaceholder.typicode.com/posts";
-       console.log("entra");
       let result = await axios({
       url1,
       method: 'GET',
       dataType : 'json',
       ContentType: 'application/json',
       });
-      console.log(result.data);
-      updPst( result.data);
+      updAl( result.data);
     }
     catch(error)
     {
@@ -29,8 +27,9 @@ function App() {
     }
   }
   
-  const getImgList = async () => {
+  const getImagenes = async () => {
     try {
+      
       let url2="https://jsonplaceholder.typicode.com/photos";
       let result = await axios({
         url2,
@@ -38,8 +37,7 @@ function App() {
         dataType : 'json',
         ContentType: 'application/json',
         });
-        console.log(result.data);
-        updImgs(result.data);
+        updPro(result.data);
     } catch (error) {
       alert(error); 
     }
@@ -49,32 +47,32 @@ function App() {
     <div className="container">
       <div className='row'>
         <div className='col-md-12'>
-          <button className='btn btn-primary' onClick={getPostList}>Posts</button>
-          <button className='btn btn-success' onClick={getImgList}>Images</button>
+          <button className='btn btn-primary' onClick={getPosts}>Mostrar Post</button>
+          <button className='btn btn-success' onClick={getImagenes}>Mostrar Imagenes</button>
         </div>
       </div>
       <div className='row'>
         <div className='col-md-12'>
-          <table className='table'>
+          <table className='table table-dark'>
               <thead>
                 <tr>
-                  <th colSpan={4} >POST</th>
+                  <th colSpan={4} >Posts</th>
                 </tr>
                 <tr>
-                  <th scope='col'> User ID </th>
+                  <th scope='col'> Usuario ID</th>
                   <th scope='col'> ID</th>
-                  <th scope='col'> Title</th>
-                  <th scope='col'> Body</th>
+                  <th scope='col'> Titulo</th>
+                  <th scope='col'> Cuerpo</th> 
                 </tr>
               </thead>
               <tbody>
               {
-                post.map( (data)=> 
-                <tr key={"celda"+data.userId}>
-                  <td key={"userID"+data.userId}>{data.userId}</td> 
-                  <td key={"ID"+data.userId}>{data.id}</td>
-                  <td key={"title"+data.userId}>{data.title} </td>
-                  <td key={"body"+data.userId}>{data.body}</td>
+                posts.map( (datos)=> 
+                <tr key={"celda"+datos.userId}>
+                  <td key={"userID"+datos.userId}>{datos.userId}</td> 
+                  <td key={"ID"+datos.userId}>{datos.id}</td>
+                  <td key={"title"+datos.userId}>{datos.title} </td>
+                  <td key={"body"+datos.userId}>{datos.body}</td>
                 </tr>
                 )
               }
@@ -83,14 +81,14 @@ function App() {
         </div>   
       </div>
       <div className='row'>
-        <div className='col-md-12 header-photo'>PHOTO</div>
+        <div className='col-md-12 header-photo'>IMÁGENES</div>
       </div>
       <div className='row'>
       {
-        imgs.map( (data)=> 
-        <div key={"id:"+data.albumId} className='col-md-4 bg-info'>
-          <p key={"title:"+data.title}> Title: {data.title}</p>
-          <img key={"source:"+data.url} src={data.url} alt={"Albúm ID:"+data.albumId+"ID:"+data.id+"thumbnailUrl:"+data.thumbnailUrl}/>
+        imagenes.map( (datos)=> 
+        <div key={"id:"+datos.albumId} className='col-md-4'>
+          <p key={"title:"+datos.title}> Titulo: {datos.title}</p>
+          <img key={"source:"+datos.url} size="200px"  src={datos.url} alt={"Albúm ID:"+datos.albumId+"ID:"+datos.id+"thumbnailUrl:"+datos.thumbnailUrl}/>
         </div>
         )
       }
@@ -98,5 +96,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
